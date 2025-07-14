b0fc9b50eb4e311c909e21905052da242e1cc008
AlSamp <Alix@kwiktic.com>
2025-07-14 16:11:55 +0100

    Temporary TenantId Selection availabe in register

---
+++ KwikTicMaterialBatch/Utilities/TenantId.cs
@@ -0,0 +1,19 @@
+﻿namespace KwikTicMaterialBatch.Utilities
+{
+    public static class TenantId
+    {
+        public static readonly Dictionary<string, string> TenantIdMap = new()
+        {
+            { "Tenant1", "c1a07c8b-1c2b-4b91-8df9-3a9fbc6ed111" },
+            { "Tenant2", "a2f3080d-e9e1-4c91-8e60-f7d2e9334b4f" },
+            { "Tenant3", "cddcb011-0d90-44b9-a079-4db0f8d4574c" },
+            { "Tenant4", "14d76176-4c32-41e3-aea8-06d233a3a5cc" },
+            { "Tenant5", "8e3d2b59-e62a-4e3c-8cb4-4df91d8ab001" },
+            { "Tenant6", "9f70e68a-33e5-4c4b-9f18-25e6fba93794" },
+            { "Tenant7", "71aa8b42-84e3-4452-85d7-9b88d5a8cc2f" },
+            { "Tenant8", "4b6bcf68-9f71-495d-80c6-c3009b4a37c0" },
+            { "Tenant9", "3c68a927-89b3-4669-8a4e-68a3d6404ea5" },
+            { "Tenant10", "f40358c4-8424-471e-a7df-70fd0e12163a" }
+        };
+    }
+}
